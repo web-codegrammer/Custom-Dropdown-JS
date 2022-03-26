9b51946fbceb904bc64e25f6b2b691092a362c67
Devanshu Vashishtha <sharmadevanshu452@gmail.com>
2022-03-26 19:19:33 +0530

    Finished development

---
+++ script.js
@@ -0,0 +1,9 @@
+import Select from './select.js'
+
+const selectElements = document.querySelectorAll('[data-custom]')
+
+selectElements.forEach(selectElement => {
+   new Select(selectElement)
+})
+
+
